044cc3fce817c66dd12121a391020c619b282090
ZhaoTim <null>
2025-01-12 04:57:32 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [2](https://ZhaoTim.github.io/tag.html) 
 ### :speech_balloon: 1 
 ### :hibiscus: 407 
-### :alarm_clock: 2025-01-12 12:52:24 
+### :alarm_clock: 2025-01-12 12:57:32 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/cal-heatmap.js
+++ docs/cal-heatmap.js
@@ -1,6 +1,8 @@
-const cal = new CalHeatmap();
-cal.paint({});
+setTimeout(() => {
 const contentContainer = document.querySelector('#content');
 const container = document.createElement('div');
 container.id = 'cal-heatmap'
 contentContainer.append(container);
+const cal = new CalHeatmap();
+cal.paint({});
+}, 3000)
