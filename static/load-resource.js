90ddb5abc4f2820b58e62f413e64ac7f79abbd13
zhaomeicheng <30540533+ZhaoTim@users.noreply.github.com>
2025-01-13 17:30:38 +0800

    Update load-resource.js

--- static/load-resource.js
+++ static/load-resource.js
@@ -1,11 +1,28 @@
+(function() {
+            const inlineCss = `
+                * {
+                    font-family: 'GuanKiapTsingKhai' !important;
+                }
+                #postBody {
+                    font-size: 22px;
+                    line-height: 1.9;
+                }
+            `;
 
-        document.addEventListener('DOMContentLoaded', () => {
+            const styleElement = document.createElement('style');
+            styleElement.textContent = inlineCss;
+            document.head.appendChild(styleElement);
+        })();
+document.addEventListener('DOMContentLoaded', () => {
             function loadResources(urls) {
                 return Promise.all(
                     urls.map((url) => {
                         return new Promise((resolve, reject) => {
                             let element;
-                            if (url.endsWith('.js')) {
+                                const knownScriptUrls = [
+                            'https://unpkg.com/@popperjs/core@2'
+                        ];
+                            if (url.endsWith('.js')  || knownScriptUrls.includes(url)) {
                                 element = document.createElement('script');
                                 element.src = url;
                                 element.async = true; // Use async for non-blocking
@@ -27,9 +44,15 @@
                 );
             }
 
+                <link rel='stylesheet' href='' /><script src=''></script><script src=''></script><link rel='stylesheet' href=''><style>*{font-family:'GuanKiapTsingKhai' !important;}#postBody{font-size: 22px;line-height: 1.9;}</style><script src=''></script><script src=''></script><script src=''></script><script src=''></script>
+
             loadResources([
-                'https://example.com/script1.js',
-                'https://example.com/style1.css'
+                'https://chinese-fonts-cdn.deno.dev/packages/GuanKiapTsingKhai/dist/GuanKiapTsingKhai/result.css',
+                'https://d3js.org/d3.v7.min.js',
+                'https://unpkg.com/cal-heatmap/dist/cal-heatmap.min.js',
+                'https://unpkg.com/cal-heatmap/dist/plugins/Tooltip.min.js',
+                'https://unpkg.com/cal-heatmap/dist/plugins/CalendarLabel.min.js',
+                'https://unpkg.com/cal-heatmap/dist/plugins/LegendLite.min.js'      
             ]).then(() => {
                 console.log('All resources loaded successfully.');
             }).catch((error) => {
