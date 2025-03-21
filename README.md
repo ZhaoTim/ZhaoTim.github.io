cddb9f7dec36c7712e694c608a29a5af3f1206f8
ZhaoTim <null>
2025-03-21 14:12:49 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Tim :link: https://ZhaoTim.github.io 
-### :page_facing_up: [7](https://ZhaoTim.github.io/tag.html) 
-### :speech_balloon: 5 
+### :page_facing_up: [8](https://ZhaoTim.github.io/tag.html) 
+### :speech_balloon: 6 
 ### :hibiscus: 2655 
-### :alarm_clock: 2025-03-20 20:49:27 
+### :alarm_clock: 2025-03-21 22:12:48 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/load-resource.js
+++ docs/load-resource.js
@@ -1,10 +1,7 @@
-(function() {
-            const inlineCss = `
-@import url('https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@200..900&family=Roboto+Slab:wght@100..900&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Shafarik&display=swap');
-
+(function () {
+    const inlineCss = `
 * {
- 	font-family: 'Shafarik', 'FZFW ZhuZi MinchoS RB' !important;
+ 	font-family: 'FZFW ZhuZi A Old Mincho R' !important;
 	font-optical-sizing: auto;
 }
 
@@ -23,48 +20,44 @@ video {
 }
             `;
 
-            const styleElement = document.createElement('style');
-            styleElement.textContent = inlineCss;
-            document.head.appendChild(styleElement);
-        })();
+    const styleElement = document.createElement('style');
+    styleElement.textContent = inlineCss;
+    document.head.appendChild(styleElement);
+})();
 
-            function loadResources(urls) {
-                return Promise.all(
-                    urls.map((url) => {
-                        return new Promise((resolve, reject) => {
-                            let element;
-                                const knownScriptUrls = [
-                            'https://unpkg.com/@popperjs/core@2'
-                        ];
-                            if (url.endsWith('.js')  || knownScriptUrls.includes(url)) {
-                                element = document.createElement('script');
-                                element.src = url;
-                                element.async = true; // Use async for non-blocking
-                                element.onload = () => resolve(url);
-                                element.onerror = () => reject(new Error(`Failed to load script: ${url}`));
-                            } else if (url.endsWith('.css')) {
-                                element = document.createElement('link');
-                                element.href = url;
-                                element.rel = 'stylesheet';
-                                element.onload = () => resolve(url);
-                                element.onerror = () => reject(new Error(`Failed to load stylesheet: ${url}`));
-                            } else {
-                                reject(new Error(`Unsupported file type: ${url}`));
-                                return;
-                            }
-                            document.head.appendChild(element);
-                        });
-                    })
-                );
-            }
+function loadResources(urls) {
+    return Promise.all(
+        urls.map((url) => {
+            return new Promise((resolve, reject) => {
+                let element;
+                const knownScriptUrls = [
+                    'https://unpkg.com/@popperjs/core@2'
+                ];
+                if (url.endsWith('.js') || knownScriptUrls.includes(url)) {
+                    element = document.createElement('script');
+                    element.src = url;
+                    element.async = true; // Use async for non-blocking
+                    element.onload = () => resolve(url);
+                    element.onerror = () => reject(new Error(`Failed to load script: ${url}`));
+                } else if (url.endsWith('.css')) {
+                    element = document.createElement('link');
+                    element.href = url;
+                    element.rel = 'stylesheet';
+                    element.onload = () => resolve(url);
+                    element.onerror = () => reject(new Error(`Failed to load stylesheet: ${url}`));
+                } else {
+                    reject(new Error(`Unsupported file type: ${url}`));
+                    return;
+                }
+                document.head.appendChild(element);
+            });
+        })
+    );
+}
 
 
-            loadResources([
-     'https://chinese-fonts-cdn.deno.dev/packages/hwmct/dist/%E6%B1%87%E6%96%87%E6%98%8E%E6%9C%9D%E4%BD%93/result.css',
-		    'https://zhaotim.github.io/fonts/font.css',
-		    'https://zhaotim.github.io/jinghua/result.css',
-		    'https://zhaotim.github.io/fangzhengfwzhuzimingchaojianrb/result.css',
-		    'https://calm-panda-3cb350.netlify.app/result.css',
-            ]);
- 
-  
+loadResources([
+    'https://zhaotim.github.io/fangzhengfwzhuzialaomingchaojianr/result.css',
+]);
+
+
